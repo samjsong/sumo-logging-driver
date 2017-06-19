5920283ee023d336619687c34e9eb9e4027e142c
34350NAME <34350EMAIL>
2017-06-19 13:22:58 -0700

    create working bare-bones implementation and tests

---
+++ sumologs.go
@@ -0,0 +1,217 @@
+// Package sumologs provides the log driver for forwarding server logs to
+// SumoLogic HTTP Source endpoint.
+package sumologs
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/daemon/logger"
+)
+
+const (
+	driverName = "sumologs"
+	logOptUrl = "sumo-url"
+
+	defaultFrequency = 5 * time.Second
+	defaultBatchSize = 1000
+	defaultBufferSize = 10 * defaultBatchSize
+	defaultStreamSize = 4 * defaultBatchSize
+)
+
+type sumoLogger struct {
+	client    *http.Client
+	
+	httpSourceUrl string
+
+	frequency  time.Duration
+	batchSize  int
+	bufferSize int
+	streamSize int
+
+	messageStream chan string
+	mu            sync.RWMutex
+	readyToClose  bool
+	closedCond    *sync.Cond
+}
+
+func init() {
+	if err := logger.RegisterLogDriver(driverName, New); err != nil {
+		logrus.Fatal(err)
+	}
+	if err := logger.RegisterLogOptValidator(driverName, ValidateLogOpt); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func New(info logger.Info) (logger.Logger, error) {
+	httpSourceUrl, ok := info.Config[logOptUrl]
+	if !ok {
+		return nil, fmt.Errorf("%s: %s is required", driverName, logOptUrl)
+	}
+
+	// can allow users to configure these variables in future
+	frequency := defaultFrequency
+	batchSize := defaultBatchSize
+	bufferSize := defaultBufferSize
+	streamSize := defaultStreamSize
+
+	s := &sumoLogger{
+		client: &http.Client{}
+		httpSourceUrl: httpSourceUrl,
+		messageStream: make(chan string, streamSize),
+		frequency: frequency,
+		batchSize: batchSize,
+		bufferSize: bufferSize,
+	}
+
+	go s.waitForMessages()
+
+	return s, nil
+}
+
+func (s *sumoLogger) waitForMessages() {
+	// TODO: Eventually multiline detection should probably happen here...
+	//		Might get rid of ticker and send messages immediately instead
+	// TODO: Discuss design, should we send messages in batches? Fewer http requests
+	//		but could potentially send unrelated messages as a single log.
+	//		Potential solution would involve changing things on the http-source side,
+	//		to parse when unrelated messages are bundled together, but this could
+	//		cause complications with multiline detection. Probably easier to just send
+	//		one message per log.
+	timer := time.NewTicker(s.frequency)
+	var messages []string
+	for {
+		select {
+		case <-timer.C:
+			messages = s.sendMessages(messages, false)
+		case message, open := <-s.messageStream:
+			if !open {
+				s.sendMessages(messages, true)
+				s.mu.Lock()
+				defer s.mu.Unlock()
+				s.readyToClose = true
+				s.closedCond.Signal()
+				return
+			}
+			messages = append(messages, message)
+			// maybe check if multiline is done, then send as one log.
+			if len(messages) % s.batchSize == 0 {
+				messages = s.sendMessages(messages, false)
+			}
+		}
+	}
+}
+
+func (s *sumoLogger) Log(msg *logger.Message) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if s.closedCond != nil {
+		return fmt.Errorf("%s: driver is closed", driverName)
+	}
+	s.messageStream <- string(msg.Line)
+	logger.PutMessage(msg)
+	return nil
+}
+
+func (s *sumoLogger) sendMessages(messages []string, driverClosed bool) []string {
+	messagesCount := len(messages)
+	for i := 0; i < messagesCount; i += s.batchSize {
+		upperBound := i + s.batchSize
+		if upperBound > messagesCount {
+			upperBound = messagesCount
+		}
+		messagesToSend := messages[i:upperBound]
+		if err := s.trySendMessages(messagesToSend); err != nil {
+			// failed to send the messages
+			// TODO: if the driver is closed or the buffer is full, then need to do something with the logs
+			logrus.Error(err)
+			return messages[i:messagesCount]
+		}
+	}
+	return messages[:0]
+}
+
+func (s *sumoLogger) trySendMessages(messages []string) error {
+	if len(messages) == 0 {
+		return nil
+	}
+	// TODO: this function in general can probably be cleaned up,
+	//		need to update this function and sendMessages to do multiline detection.
+	//		currently, it sends each message one at a time, and so "batch size" is irrelevant.
+	//		need to figure out what the best design for sending messages is.
+	//		SEE: todo in waitForMessages()
+	for _, message := range messages {
+		req, err := http.NewRequest("POST", s.httpSourceUrl, bytes.NewBuffer([]byte(message)))
+		if err != nil {
+			return err
+		}
+		res, err := s.client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			var body []byte
+			body, err = ioutil.ReadAll(res.Body)
+			if err != nil {
+				return err
+			}
+			return fmt.Errorf("%s: failed to send event - %s - %s", driverName, res.Status, body)
+		}
+		io.Copy(ioutil.Discard, res.Body)
+	}
+	return nil
+}
+
+func ValidateLogOpt(cfg map[string]string) error {
+	for key := range cfg {
+		switch key {
+		case logOptUrl:
+			if cfg[key] == "" {
+				return fmt.Errorf("%s: log-opt %s cannot be empty", driverName, key)
+			}
+		case "labels":
+		case "env":
+		case "env-regex":
+		default:
+			return fmt.Errorf("%s: unknown log-opt '%s'", driverName, key)
+		}
+	}
+	return nil
+}
+
+func (s *sumoLogger) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.closedCond == nil {
+		s.closedCond = sync.NewCond(&s.mu)
+		close(s.messageStream)
+		for !s.readyToClose {
+			s.closedCond.Wait()
+		}
+	}
+	return nil
+}
+
+func (s *sumoLogger) Name() string {
+	return driverName
+}
+
+/* 
+-----------------------------------------------------------------------------
+Changes I made to existing files:
+-----------------------------------------------------------------------------
+api/swagger.yaml 553
+contrib/completion/bash/docker 743 767 803-805 899-904
+contrib/completion/zsh/_docker 224 236 247 255
+daemon/logdrivers_linux.go 14
+daemon/logdrivers_windows.go 12
+
+*/ 
---
+++ sumologs_test.go
@@ -0,0 +1,113 @@
+package sumologs
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/docker/docker/daemon/logger"
+
+	// run "go get gopkg.in/jarcoal/httpmock.v1" before running tests
+	"gopkg.in/jarcoal/httpmock.v1"
+)
+
+const (
+	sumoUrl = "https://stag-events.sumologic.net/receiver/v1/http/ZaVnC4dhaV27p86wEkCthvRI4IUASAom3K3-y2qQI8aLQuMKT8wL4yrXruk4ak1UUk10h4LY7-w9Jcb1yb7a5rSdx2-KkLN48eeyR6eqE17ygZut36dfJQ=="
+	sumoUrlMock = "https://fake.sumo.Url"
+)
+
+func TestValidateLogOpt(t *testing.T) {
+	assert := assert.New(t)
+	err := ValidateLogOpt(map[string]string{
+		logOptUrl : sumoUrl,
+	})
+	assert.Nil(err)
+
+	err = ValidateLogOpt(map[string]string{
+		"bad-option" : "bad-option-value",
+	})
+	assert.NotNil(err, "should fail when user enters unsupported option")
+
+	err = ValidateLogOpt(map[string]string{
+		logOptUrl : "",
+	})
+	assert.NotNil(err, "should fail when user enters empty %s", logOptUrl)
+}
+
+func TestNew(t *testing.T) {
+	assert := assert.New(t)
+	t.Run("missing sumo-url", func(t *testing.T) {
+		info := logger.Info{
+			Config: map[string]string{},
+		}
+		_, err := New(info)
+
+		assert.NotNil(err, "should fail when sumo-url not provided")
+	})
+	t.Run("configured correctly", func(t *testing.T) {
+		info := logger.Info{
+			Config: map[string]string{
+				"sumo-url" : sumoUrl,
+			},
+		}
+
+		s, err := New(info)
+		assert.Nil(err, "should not fail when configured correctly")
+		
+		assert.Equal(driverName, s.Name(), "name of logging driver is incorrect")
+		
+		err = s.Close()
+		assert.Nil(err, "should not fail when calling Close() on logger")
+	})
+}
+
+func TestDefaultSettings(t *testing.T) {
+	assert := assert.New(t)
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	var messages []string
+
+	httpmock.RegisterResponder("POST", sumoUrlMock,
+		func(req *http.Request) (*http.Response, error) {
+			fmt.Println("got the request")
+
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(req.Body)
+			message := buf.String()
+
+			messages = append(messages, message)
+			resp := httpmock.NewStringResponse(200, message)
+			return resp, nil
+		},
+	)
+	info := logger.Info{
+		Config: map[string]string{
+			"sumo-url" : sumoUrlMock,
+		},
+	}
+
+	s, err := New(info)
+	assert.Nil(err, "should not fail when calling New()")
+
+	msgStrings := []string{"hello", "", "This is a log with\na 2nd line and a #!"}
+
+	var msg *logger.Message
+	for _, msgString := range msgStrings {
+		msg = &logger.Message{Line: []byte(msgString), Source: "stdout", Timestamp: time.Now()}
+		err = s.Log(msg)
+		assert.Nil(err, "should not fail when calling Log(). sent %s", msgString)
+	}
+
+	err = s.Close()
+	assert.Nil(err, "should not fail when calling Close()")
+
+	assert.Equal(3, len(messages), "should have received 3 logs")
+	for i := 0; i < 3; i++ {
+		assert.Equal(msgStrings[i], messages[i], "message is incorrect")
+	}
+}
